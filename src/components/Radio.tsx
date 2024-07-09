dec6a576b0ad372c0b9d486e231f6470bc301544
Jang Yoonsung <upthe15752@gmail.com>
2024-07-10 02:33:43 +0900

    feat: radio 컴포넌트 추가

---
+++ src/components/Radio.tsx
@@ -0,0 +1,45 @@
+import { TSchema } from '@/container/CardForm';
+import '@/style/radio.css';
+import { Fragment } from 'react';
+import { FieldErrors, Path, UseFormRegister } from 'react-hook-form';
+
+const Radio = <T extends string>({
+  values,
+  name,
+  errors,
+  register,
+  labels,
+  classes,
+}: {
+  values: readonly T[];
+  name: Path<TSchema>;
+  errors: FieldErrors<TSchema>;
+  register: UseFormRegister<TSchema>;
+  labels?: Record<T, string>;
+  classes?: string;
+}) => {
+  return (
+    <>
+      <div role="radioGroup" className={`flex gap-2 text-sm ${classes}`}>
+        {values.map((value, index) => {
+          const id = `${value}-${index}`;
+          return (
+            <Fragment key={index}>
+              <label htmlFor={id} className="flex items-center gap-1">
+                <input id={id} className="radio" type="radio" value={value} {...register(name)} />
+                {labels?.[value] ?? value}
+              </label>
+            </Fragment>
+          );
+        })}
+      </div>
+      {errors?.[name]?.message && (
+        <div className="pt-1 text-xs">
+          <p className="text-red-600">{errors[name].message}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Radio;
