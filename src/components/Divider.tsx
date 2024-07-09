ad42a287ae8ad141156f3db9684430612c078feb
Jang Yoonsung <upthe15752@gmail.com>
2024-07-10 01:15:36 +0900

    feat: props

--- src/components/Divider.tsx
+++ src/components/Divider.tsx
@@ -5,15 +5,20 @@ import { CSSProperties } from 'react';
 const Divider = ({
   orientation = 'horizontal',
   thickness = '1px',
+  color = 'lightGray',
+  classes,
 }: {
   orientation?: (typeof ORIENTATION)[keyof typeof ORIENTATION];
   thickness?: string;
+  color?: string;
+  classes?: string;
 }) => {
   const style: CSSProperties = {
     ['--thickness' as string]: thickness,
+    ['--color' as string]: color,
   };
 
-  return <div data-orientation={orientation} style={style} className="root" />;
+  return <div data-orientation={orientation} style={style} className={`root ${classes}`} />;
 };
 
 export default Divider;
