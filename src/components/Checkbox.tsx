efcaeb84eb62ffb2313b60a71d8729978c38c5be
Jang Yoonsung <upthe15752@gmail.com>
2024-07-10 20:26:05 +0900

    feat: 일단 체크박스 가구현

---
+++ src/components/Checkbox.tsx
@@ -0,0 +1,37 @@
+import { TSchema } from '@/container/CardForm';
+import '@/style/checkbox.css';
+import React from 'react';
+import { FieldErrors, Path, UseFormRegister } from 'react-hook-form';
+
+const Checkbox = ({
+  name,
+  register,
+  errors,
+  children,
+  classes,
+}: {
+  name: Path<TSchema>;
+  register: UseFormRegister<TSchema>;
+  errors: FieldErrors<TSchema>;
+  children?: React.ReactNode;
+  classes?: string;
+}) => {
+  return (
+    <>
+      <label
+        htmlFor={name}
+        className={`inline-flex gap-2 relative items-center text-sm cursor-pointer ${classes}`}>
+        <input type="checkbox" className="hidden" id={name} {...register(name)} />
+        <span className="checkbox" />
+        {children}
+      </label>
+      {errors?.[name]?.message && (
+        <div>
+          <p>{errors[name].message}</p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Checkbox;
