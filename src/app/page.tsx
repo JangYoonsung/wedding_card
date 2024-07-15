e946f1b24dd34ae79191da3933542b6781683559
Jang Yoonsung <upthe15752@gmail.com>
2024-07-16 01:17:40 +0900

    feat: 가작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
+import Divider from '@/components/Divider';
 import LinkButton from '@/components/LinkButton';
 import Map from '@/container/Map';
 
 const Home = () => {
   return (
-    <main className="m-4">
-      <Map />
+    <main className="m-4 h-svh">
+      <section className="my-4 text-center">
+        <div className="font-bold text-lg text-primary">
+          <h2>LOCATION INFO</h2>
+          <h2>アクセス</h2>
+        </div>
+        <Divider classes="my-2" />
+        <div className="whitespace-pre">
+          <p className="text-sm text-gray pb-4">{`セントアクアチャペル市ヶ谷
+
+東京都新宿区市谷仲之町3-5 
+アプローズスクエア東京迎賓館 内
+
+03-6402-7256`}</p>
+          <a
+            href="https://www.saint-acqua-chapel.jp/place/ichigaya/"
+            target="_blank"
+            className="text-sm text-gray underline">
+            www.saint-acqua-chapel.jp/place/ichigaya/
+          </a>
+        </div>
+        <Divider classes="my-2" />
+        <Map />
+      </section>
+
       <LinkButton href="/reply-form">招待状に回答する</LinkButton>
     </main>
   );
