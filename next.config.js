82a91027eb228dac2c98de07699a2bfc69e53b77
Jang Yoonsung <upthe15752@gmail.com>
2024-07-15 15:40:27 +0900

    chore: next config add svg

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [{ loader: '@svgr/webpack' }],
+    });
+    return config;
+  },
+  images: { disableStaticImages: true },
 };
 
 module.exports = nextConfig;
