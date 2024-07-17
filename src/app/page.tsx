3979534f377bb5c8e2c59ef2ac2274bc87989e84
Jang Yoonsung <upthe15752@gmail.com>
2024-07-18 01:50:37 +0900

    style: style fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,44 +6,39 @@ import Map from '@/container/Map';
 
 const Home = () => {
   return (
-    <main className="h-full bg-primary">
-      <Card useAnimation showShadow classes="m-4">
-        <div className="font-bold text-lg text-primary">
-          <h2>WEDDING INFORMATION</h2>
-          <h2>挙式・披露宴</h2>
-        </div>
+    <main className="h-full bg-primary grid gap-6">
+      <Card useAnimation showShadow classes="text-primary mt-4 mx-4">
+        <h2 className="font-bold text-xl ">WEDDING INFORMATION</h2>
         <Divider classes="my-2" />
 
         <div>
-          <h2 className="text-lg font-bold text-primary">日付</h2>
+          <h2 className="text-lg font-bold">日付</h2>
           <p className="text-lg text-gray">2024年09月06日（金）</p>
         </div>
         <Divider classes="my-2" />
 
         <div>
-          <h2 className="text-lg font-bold text-primary">挙式</h2>
+          <h2 className="text-lg font-bold">挙式</h2>
           <p className="text-lg text-gray">午後12:30</p>
         </div>
         <Divider classes="my-2" />
 
         <div>
-          <h2 className="text-lg font-bold text-primary">披露宴</h2>
+          <h2 className="text-lg font-bold">披露宴</h2>
           <p className="text-lg text-gray">午後13:30</p>
         </div>
         <Divider classes="my-2" />
 
         <div className="break-words whitespace-pre-wrap py-2">
-          <p className="text-gray text-sm">{`※ご多用中恐縮に存じますが挙式にもご列席を賜りたく
+          <p className="text-sm text-gray">{`※ご多用中恐縮に存じますが挙式にもご列席を賜りたく
 開式の30分前までにお越しくださいますようお願い申し上げます`}</p>
         </div>
       </Card>
 
-      <Card classes="m-4" useAnimation showShadow>
-        <div className="font-bold text-lg text-primary">
-          <h2>LOCATION INFO</h2>
-          <h2>アクセス</h2>
-        </div>
+      <Card classes="mx-4" useAnimation showShadow>
+        <h2 className="font-bold text-xl text-primary">LOCATION INFO</h2>
         <Divider classes="my-2" />
+
         <div className="whitespace-pre">
           <p className="text-sm text-gray pb-4">{`セントアクアチャペル市ヶ谷
 
@@ -64,11 +59,11 @@ const Home = () => {
         <Map />
       </Card>
 
-      <Card color="primary" classes="form mt-8 mb-[15rem]" useAnimation>
-        <h2 className="text-white font-bold text-lg">RSVP</h2>
+      <Card color="primary" classes="text-white mb-[15rem]" useAnimation>
+        <h2 className="font-bold text-xl">RSVP</h2>
         <Divider classes="my-2" />
         <div className="whitespace-pre">
-          <p className="text-sm text-white py-4">{`ご出欠のご返信は郵送に変わり
+          <p className="text-sm py-4">{`ご出欠のご返信は郵送に変わり
 下記ボタンよりフォームへアクセスいただき
 ご返信をお願い申し上げます`}</p>
         </div>
