cd1a77a8fe625d3b0020565604bb385f84e97701
Jang Yoonsung <upthe15752@gmail.com>
2024-07-19 01:11:58 +0900

    style: css 파일 수정

--- src/container/Top.tsx
+++ src/container/Top.tsx
@@ -1,16 +1,18 @@
 import Card from '@/components/Card';
 import Title from '@/components/Title';
+import '@/style/animation.css';
+import '@/style/top.css';
 
 const Top = () => {
-  const textStyle = 'text-white text-left text-lg text-shadow w-fit hidden whitespace-nowrap';
+  const textStyle = 'text-white text-left text-lg text-shadow';
 
   return (
-    <Card classes="h-screen flex items-center justify-center !bg-cover before:content-[''] before:w-full before:h-full before:min-h-[30rem] before:absolute before:top-o before:left-0">
+    <Card classes="top-container">
       <div className="z-10 font-bold">
-        <Title text="YOONSUNG & JUAN" color="white" classes="text-4xl text-center" />
-        <p className={`${textStyle} mt-4`}>WE'RE GETTING MARRIED</p>
-        <p className={`${textStyle}`}>PLEASE US TO CELEBRATION</p>
-        <p className={`${textStyle} mt-4`}>2024. 09. 06. FRI PM12:30</p>
+        <Title text="YOONSUNG & JUAN" color="white" classes="typing" />
+        <p className="top-title mt-4">WE'RE GETTING MARRIED</p>
+        <p className="top-title">PLEASE US TO CELEBRATION</p>
+        <p className="top-title mt-4">2024. 09. 06. FRI PM12:30</p>
       </div>
     </Card>
   );
