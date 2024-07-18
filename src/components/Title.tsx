61fbe0386b07c3402436a3e4290986b039bcc677
Jang Yoonsung <upthe15752@gmail.com>
2024-07-19 02:59:35 +0900

    fix: 불필요한 코드 제거

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -16,11 +16,9 @@ const Title = ({
   const style: CSSProperties = {
     ['--color' as string]: `var(--${color})`,
   };
-  const textSize = `text-${fontSize}`;
-  console.log(textSize);
 
   return (
-    <h1 className={`root-title ${textSize} ${classes}`} style={style}>
+    <h1 className={`root-title text-${fontSize} ${classes}`} style={style}>
       {text}
     </h1>
   );
