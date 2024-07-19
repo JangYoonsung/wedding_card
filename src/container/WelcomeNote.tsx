845315d2437ee05a364a3df9b1395ac4fcb3e32a
Jang Yoonsung <upthe15752@gmail.com>
2024-07-19 21:27:37 +0900

    fix: 인삿말 수정

--- src/container/WelcomeNote.tsx
+++ src/container/WelcomeNote.tsx
@@ -6,17 +6,19 @@ const WelcomeNote = () => {
 皆様におかれましては
 ますますご清祥のこととお慶び申し上げます
   
-このたび　私たちは結婚をすることになりました
-つきましては　日頃お世話になっております皆様に
-感謝を込めて　ささやかな小宴を催したく存じます
+籍を入れて2年という月日が経ちますが
+この度私たちは結婚式を挙げることになりました
+
+つきましては日頃お世話になっております皆様に
+感謝を込めてささやかな小宴を催したく存じます
   
-ご多用中　誠に恐縮ではございますが
-ぜひご出席をいただきたく　ご案内申し上げます
+ご多用中誠に恐縮ではございますが
+ぜひご出席をいただきたくご案内申し上げます
   
 謹白`;
   return (
     <Card color="none" classes="py-10" useAnimation>
-      <p className="text-white text-center text-lg whitespace-pre-wrap break-words text-shadow">
+      <p className="text-white text-center text-base whitespace-pre-wrap break-words text-shadow">
         {greetingText}
       </p>
     </Card>
