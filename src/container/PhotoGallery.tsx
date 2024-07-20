3ac20c4fd64fb70ca6a1df67fd0a6d6cd1126f8d
Jang Yoonsung <upthe15752@gmail.com>
2024-07-20 23:16:00 +0900

    fix: 슬라이더 괜찮을수도?

--- src/container/PhotoGallery.tsx
+++ src/container/PhotoGallery.tsx
@@ -1,6 +1,11 @@
+'use client';
+
 import Card from '@/components/Card';
 import Title from '@/components/Title';
 import Image from 'next/image';
+import Slider, { Settings } from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 const PhotoGallery: React.FC = () => {
   const images = [
@@ -15,22 +20,42 @@ const PhotoGallery: React.FC = () => {
     { id: 9, value: '/image/eight.jpg' },
   ];
 
+  const settings: Settings = {
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+  };
+
   return (
     <Card color="none" useAnimation>
       <Title text="Photo Gallery" color="gray" classes="font-play-fair mb-2" fontSize="2xl" />
 
-      <div className="grid grid-cols-3 gap-2">
+      <Slider
+        className="aspect-w-1 aspect-h-1"
+        dots={settings.dots}
+        arrows={settings.arrows}
+        infinite={settings.infinite}
+        adaptiveHeight={settings.adaptiveHeight}
+        autoplay={settings.autoplay}
+        speed={settings.speed}
+        slidesToShow={settings.slidesToShow}
+        slidesToScroll={settings.slidesToScroll}>
         {images.map((image) => (
-          <div key={image.id} className="aspect-w-1 aspect-h-1">
+          <div className="aspect-w-1 aspect-h-1" key={image.id}>
             <Image
               src={image.value}
               alt={`${image.id}-${image.value}`}
-              className="object-cover w-full h-full"
+              className="object-cover"
               fill
             />
           </div>
         ))}
-      </div>
+      </Slider>
     </Card>
   );
 };
