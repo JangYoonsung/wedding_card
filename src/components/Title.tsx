a92f957b6b687c15c1647d015007a6607813778a
Jang Yoonsung <upthe15752@gmail.com>
2024-07-22 01:23:45 +0900

    fix: 버그수정

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,22 +1,18 @@
 'use client';
 
 import { COLOR } from '@/constants/common';
+import { MATCHED_SIZE } from '@/constants/title';
 import { TitleProps } from '@/types/title';
 import React, { CSSProperties } from 'react';
 
-const Title: React.FC<TitleProps> = ({
-  text,
-  classes = '',
-  fontSize = '',
-  color = COLOR.BLACK,
-}) => {
+const Title: React.FC<TitleProps> = ({ text, fontSize, classes = '', color = COLOR.BLACK }) => {
   const style: CSSProperties = {
     ['--color' as string]: `var(--${color})`,
   };
 
   return (
     <h1
-      className={`${fontSize ? `text-${fontSize}` : ''} text-[var(--color)] font-bold ${classes}`}
+      className={`${fontSize ? `${MATCHED_SIZE[fontSize]}` : ''} text-[var(--color)] font-bold ${classes}`}
       style={style}>
       {text}
     </h1>
--- src/constants/title.ts
+++ src/constants/title.ts
@@ -4,6 +4,16 @@ export const FONT_SIZE = {
   BASE: 'base',
   LG: 'lg',
   XL: 'xl',
-  ['2XL']: '2xl',
-  ['3XL']: '3xl',
-};
+  '2XL': '2xl',
+  '3XL': '3xl',
+} as const;
+
+export const MATCHED_SIZE = {
+  xs: 'text-xs',
+  sm: 'text-sm',
+  base: 'text-base',
+  lg: 'text-lg',
+  xl: 'text-xl',
+  '2xl': 'text-2xl',
+  '3xl': 'text-3xl',
+} as const;
