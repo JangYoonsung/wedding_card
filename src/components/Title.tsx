8d8bd4ed40182a40f0251e6a525732dabfc2f197
Jang Yoonsung <upthe15752@gmail.com>
2024-07-22 01:28:53 +0900

    chore: test

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { COLOR } from '@/constants/common';
-import { MATCHED_SIZE } from '@/constants/title';
 import { TitleProps } from '@/types/title';
 import React, { CSSProperties } from 'react';
 
@@ -11,9 +10,7 @@ const Title: React.FC<TitleProps> = ({ text, fontSize, classes = '', color = COL
   };
 
   return (
-    <h1
-      className={`${fontSize ? `${MATCHED_SIZE[fontSize]}` : ''} text-[var(--color)] font-bold ${classes}`}
-      style={style}>
+    <h1 className={`text-2xl text-[var(--color)] font-bold ${classes}`} style={style}>
       {text}
     </h1>
   );
