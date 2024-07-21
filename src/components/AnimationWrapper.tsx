69e7ef30e9bcbaf525f307b9f9182ff41d4bfd9f
Jang Yoonsung <upthe15752@gmail.com>
2024-07-21 12:41:03 +0900

    feat: animation wrapper components

---
+++ src/components/AnimationWrapper.tsx
@@ -0,0 +1,16 @@
+import { useVisible } from '@/hooks/useVisible';
+import { CommonProps } from '@/types/common';
+import React, { useRef } from 'react';
+
+const AnimationWrapper: React.FC<CommonProps> = ({ children }) => {
+  const divRef = useRef(null);
+  const isVisible = useVisible(divRef);
+
+  return (
+    <div ref={divRef} className={`fade-in ${isVisible ? 'show' : ''}`}>
+      {children}
+    </div>
+  );
+};
+
+export default AnimationWrapper;
