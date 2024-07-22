ad958e25d4c1761ddcc2a059422183acad7e16e8
Jang Yoonsung <upthe15752@gmail.com>
2024-07-23 00:59:26 +0900

    fix: reply form page

--- src/app/reply-form/page.tsx
+++ src/app/reply-form/page.tsx
@@ -1,7 +1,8 @@
+import Title from '@/components/Title';
 import CardForm from '@/container/CardForm';
 import React from 'react';
 
-const Home: React.FC = () => {
+const ReplyForm: React.FC = () => {
   const description = `お手数ではございますが
 下記お日にち迄に
 出欠のご回答をくださいますよう
@@ -11,16 +12,25 @@ const Home: React.FC = () => {
 ご一報いただけますと幸いです
   `;
   return (
-    <main className="m-4">
-      <h1 className="text-center text-primary font-bold">Wedding Invitation</h1>
-      <h2 className="text-center">Yoonsung & Juan</h2>
+    <main>
+      <div className="text-center my-4">
+        <Title
+          text="Wedding Invitation"
+          classes="font-play-fair"
+          fontSize="2xl"
+          color="secondary"
+        />
+        <Title text="Yoonsung & Juan" classes="font-play-fair" color="secondary" fontSize="xl" />
+      </div>
 
-      <div className="text-sm pt-4 text-center text-gray whitespace-pre">{description}</div>
+      <div className="text-sm py-2 text-center text-gray whitespace-pre">{description}</div>
 
       <p className="font-bold text-gray text-center">2024年8月9日（金）</p>
-      <CardForm />
+      <div className="mx-4">
+        <CardForm />
+      </div>
     </main>
   );
 };
 
-export default Home;
+export default ReplyForm;
