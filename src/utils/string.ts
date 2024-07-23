ecabab09a8b318ec2dbab372adf66740ee68267d
Jang Yoonsung <upthe15752@gmail.com>
2024-07-23 23:56:44 +0900

    feat: len const

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,7 +1,9 @@
+import { ZIP_CODE_LENGTH } from '@/constants/schema';
+
 export const formatWithHyphens = (str: string): string => {
   const stringLen = str.length;
 
-  if (stringLen === 7) {
+  if (stringLen === ZIP_CODE_LENGTH) {
     return `${str.slice(0, 3)}-${str.slice(3, stringLen)}`;
   }
   return `${str.slice(0, 3)}-${str.slice(3, 7)}-${str.slice(7, stringLen)}`;
