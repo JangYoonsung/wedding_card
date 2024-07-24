09455d5924e69094bfc0346b7517538a1b69dd1e
Jang Yoonsung <upthe15752@gmail.com>
2024-07-25 00:57:37 +0900

    fix: bug fix

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -3,8 +3,10 @@ import { ZIP_CODE_LENGTH } from '@/constants/schema';
 export const formatWithHyphens = (str: string): string => {
   const stringLen = str.length;
 
-  if (stringLen === ZIP_CODE_LENGTH) {
+  if (stringLen === ZIP_CODE_LENGTH && !/^\d{7}$/.test(str)) {
     return `${str.slice(0, 3)}-${str.slice(3, stringLen)}`;
   }
+
+  if (!/^0\d{9,10}$/.test(str)) return str;
   return `${str.slice(0, 3)}-${str.slice(3, 7)}-${str.slice(7, stringLen)}`;
 };
